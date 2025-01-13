455fb2f47c78f7fe76eb84f0806507cce76192a4
Javid Sadigli <cavidoffical@gmail.com>
2025-01-14 00:56:30 +0400

    Added GlobalExceptionHandler

---
+++ src/main/java/az/atlacademy/libraryadp/exception/GlobalExceptionHandler.java
@@ -0,0 +1,92 @@
+package az.atlacademy.libraryadp.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import az.atlacademy.libraryadp.model.dto.response.BaseResponse;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler 
+{
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    @ExceptionHandler(value = AuthorNotFoundException.class)
+    public BaseResponse<Void> handleAuthorNotFoundException(AuthorNotFoundException exception)
+    {
+        return BaseResponse.<Void>builder()
+                .message(exception.getMessage())
+                .success(false)
+                .status(HttpStatus.NOT_FOUND.value())
+                .build(); 
+    }
+
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    @ExceptionHandler(value = CategoryNotFoundException.class)
+    public BaseResponse<Void> handleCategoryNotFoundException(CategoryNotFoundException exception)
+    {
+        return BaseResponse.<Void>builder()
+                .message(exception.getMessage())
+                .success(false)
+                .status(HttpStatus.NOT_FOUND.value())
+                .build();
+    }
+
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    @ExceptionHandler(value = BookNotFoundException.class)
+    public BaseResponse<Void> handleBookNotFoundException(BookNotFoundException exception)
+    {
+        return BaseResponse.<Void>builder()
+                .message(exception.getMessage())
+                .success(false)
+                .status(HttpStatus.NOT_FOUND.value())
+                .build();
+    }
+
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    @ExceptionHandler(value = StudentNotFoundException.class)
+    public BaseResponse<Void> handleStudentNotFoundException(StudentNotFoundException exception)
+    {
+        return BaseResponse.<Void>builder()
+                .message(exception.getMessage())
+                .success(false)
+                .status(HttpStatus.NOT_FOUND.value())
+                .build();
+    }
+    
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    @ExceptionHandler(value = OrderNotFoundException.class)
+    public BaseResponse<Void> handleOrderNotFoundException(OrderNotFoundException exception)
+    {
+        return BaseResponse.<Void>builder()
+                .message(exception.getMessage())
+                .success(false)
+                .status(HttpStatus.NOT_FOUND.value())
+                .build();
+    }
+
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(value = FinCodeAlreadyExistsException.class)
+    public BaseResponse<Void> handleFinCodeAlreadyExistsException(FinCodeAlreadyExistsException exception)
+    {
+        return BaseResponse.<Void>builder()
+                .message(exception.getMessage())
+                .success(false)
+                .status(HttpStatus.BAD_REQUEST.value())
+                .build();
+    }
+
+    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(value = Exception.class)
+    public BaseResponse<Void> handleException(Exception exception)
+    {
+        log.error(exception.getMessage());
+        return BaseResponse.<Void>builder()
+                .message("An unexpected error occurred.")
+                .success(false)
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .build();
+    }
+}
