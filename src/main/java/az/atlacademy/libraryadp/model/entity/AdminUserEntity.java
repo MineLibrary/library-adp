c0c3ba17a0e8b6af54cc2048bf9def19f1169b7c
Javid Sadigli <cavidoffical@gmail.com>
2025-01-17 00:19:48 +0400

    Added Admin Users

---
+++ src/main/java/az/atlacademy/libraryadp/model/entity/AdminUserEntity.java
@@ -0,0 +1,31 @@
+package az.atlacademy.libraryadp.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "admin_users")
+public class AdminUserEntity 
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+}
