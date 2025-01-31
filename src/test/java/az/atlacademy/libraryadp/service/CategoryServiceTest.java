f2c7aae06d2b91952f74fac6006acdff36978ced
Javid Sadigli <cavidoffical@gmail.com>
2025-01-31 09:46:25 +0400

    Added more testing methods for CategoryService

--- src/test/java/az/atlacademy/libraryadp/service/CategoryServiceTest.java
+++ src/test/java/az/atlacademy/libraryadp/service/CategoryServiceTest.java
@@ -1,5 +1,6 @@
 package az.atlacademy.libraryadp.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
@@ -12,6 +13,7 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 
+import az.atlacademy.libraryadp.exception.CategoryNotFoundException;
 import az.atlacademy.libraryadp.mapper.CategoryMapper;
 import az.atlacademy.libraryadp.model.dto.request.CategoryRequest;
 import az.atlacademy.libraryadp.model.dto.response.BaseResponse;
@@ -32,7 +34,7 @@ public class CategoryServiceTest
     private CategoryMapper categoryMapper;
 
     @Test
-    @DisplayName(value = "Testing createCategory() method in case of correct data.")
+    @DisplayName(value = "Testing createCategory() method")
     public void givenCreateCategoryThenReturnSuccessResponse()
     {
         CategoryRequest createCategoryRequest = new CategoryRequest("drama");
@@ -41,6 +43,7 @@ public class CategoryServiceTest
 
         Mockito.when(categoryMapper.requestToEntity(createCategoryRequest)).thenReturn(createCategoryEntity); 
         Mockito.when(categoryRepository.save(createCategoryEntity)).thenReturn(createdCategoryEntity);
+        Mockito.verifyNoInteractions(categoryRepository, categoryMapper);
 
         BaseResponse<Void> serviceResponse = categoryService.createCategory(createCategoryRequest);
         
@@ -53,7 +56,7 @@ public class CategoryServiceTest
     }
 
     @Test
-    @DisplayName(value = "Testing getCategoryById() method when Category Exists")
+    @DisplayName(value = "Testing getCategoryById() method when Category exists")
     public void givenGetCategoryByIdWhenCategoryExistsThenReturnBaseResponseOfCategoryResponse()
     {
         CategoryEntity foundCategoryEntity = CategoryEntity.builder().id(1L).name("drama").build(); 
@@ -66,6 +69,7 @@ public class CategoryServiceTest
         
         Mockito.verify(categoryRepository, Mockito.times(1)).findById(1L);
         Mockito.verify(categoryMapper, Mockito.times(1)).entityToResponse(foundCategoryEntity);
+        Mockito.verifyNoMoreInteractions(categoryRepository, categoryMapper);
 
         Assertions.assertEquals(true, serviceResponse.isSuccess());
         Assertions.assertEquals("Category retrieved successfully.", serviceResponse.getMessage());
@@ -75,5 +79,62 @@ public class CategoryServiceTest
         Assertions.assertEquals("drama", serviceResponse.getData().getName());
     }
 
+    @Test
+    @DisplayName(value = "Testing getCategoryById() method when Category does not exist")
+    public void givenGetCategoryByIdWhenCategoryDoesNotExistThenThrowCategoryNotFoundException()
+    {
+        Mockito.when(categoryRepository.findById(2L)).thenReturn(Optional.empty());
+
+        CategoryNotFoundException exception = Assertions
+            .assertThrows(CategoryNotFoundException.class, () -> categoryService.getCategoryById(2L));
+            
+        Assertions.assertEquals("Category not found with id : 2", exception.getMessage());
+
+        Mockito.verify(categoryRepository, Mockito.times(1)).findById(2L);
+        Mockito.verifyNoMoreInteractions(categoryRepository, categoryMapper);
+    }
+
+    @Test
+    @DisplayName(value = "Testing getAllCategories() method")
+    public void givenGetAllCategoriesThenReturnBaseResponseOfListOfCategories()
+    {
+        List<CategoryEntity> foundCategoryEntities = List.of(
+            CategoryEntity.builder().id(1L).name("drama").build(),
+            CategoryEntity.builder().id(2L).name("comedy").build());
+
+        List<CategoryResponse> foundCategoryResponses = List.of(
+            CategoryResponse.builder().id(1L).name("drama").build(),
+            CategoryResponse.builder().id(2L).name("comedy").build());
+
+        int length = foundCategoryEntities.size();
+        
+        Mockito.when(categoryRepository.findAll()).thenReturn(foundCategoryEntities);
+        for(int i = 0; i < length; i++)
+        {
+            Mockito.when(categoryMapper.entityToResponse(foundCategoryEntities.get(i)))
+                .thenReturn(foundCategoryResponses.get(i));
+        }
+
+        BaseResponse<List<CategoryResponse>> serviceResponse = categoryService.getAllCategories();
+        
+        Mockito.verify(categoryRepository, Mockito.times(1)).findAll();
+        for(int i = 0; i < length; i++)
+        {
+            Mockito.verify(categoryMapper, Mockito.times(1))
+                .entityToResponse(foundCategoryEntities.get(i));
+        }
+        Mockito.verifyNoMoreInteractions(categoryRepository, categoryMapper);
+
+        Assertions.assertEquals("Categories retrieved successfully.", serviceResponse.getMessage());
+        Assertions.assertEquals(true, serviceResponse.isSuccess());
+        Assertions.assertEquals(HttpStatus.OK.value(), serviceResponse.getStatus());
+        Assertions.assertNotNull(serviceResponse.getData());
+        Assertions.assertEquals(length, serviceResponse.getData().size());
+        for(int i = 0; i < length; i++)
+        {
+            Assertions.assertEquals(foundCategoryResponses.get(i).getId(), serviceResponse.getData().get(i).getId());
+            Assertions.assertEquals(foundCategoryResponses.get(i).getName(), serviceResponse.getData().get(i).getName());
+        }
+    }
     
 }
