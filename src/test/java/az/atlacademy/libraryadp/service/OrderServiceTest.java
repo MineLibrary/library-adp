26127ce23effd39fb8708283be2dcae39f380bc8
Javid Sadigli <cavidoffical@gmail.com>
2025-02-15 19:04:20 +0400

    Added UNIT Tests for OrderService

--- src/test/java/az/atlacademy/libraryadp/service/OrderServiceTest.java
+++ src/test/java/az/atlacademy/libraryadp/service/OrderServiceTest.java
@@ -13,6 +13,9 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 
+import az.atlacademy.libraryadp.exception.BookNotFoundException;
+import az.atlacademy.libraryadp.exception.BookOutOfStockException;
+import az.atlacademy.libraryadp.exception.StudentNotFoundException;
 import az.atlacademy.libraryadp.mapper.OrderMapper;
 import az.atlacademy.libraryadp.model.dto.request.OrderRequest;
 import az.atlacademy.libraryadp.model.dto.response.BaseResponse;
@@ -119,5 +122,192 @@ public class OrderServiceTest
         Assertions.assertNull(serviceResponse.getData());
 
         mockedLocalDateTime.close();
+    }
+
+    @Test
+    @DisplayName(value = "Testing createOrder() method when book and student exists and book stock is enough but days to return is equal to 0")
+    public void givenCreateOrderWhenBookAndStudentExistsAndBookStockIsEnoughButDaysToReturnIsEqualToZeroThenReturnSuccessResponse()
+    {
+        int daysToReturn = 7; 
+        LocalDateTime orderTime = LocalDateTime.now();
+        LocalDateTime returnTime = orderTime.plusDays(daysToReturn); 
+        MockedStatic<LocalDateTime> mockedLocalDateTime = Mockito.mockStatic(LocalDateTime.class);
+
+        OrderRequest createOrderRequest = OrderRequest.builder()
+            .bookId(1L)
+            .studentId(1L)
+            .daysToReturn(0)
+            .build();
+
+        BookEntity createOrderBookEntity = BookEntity.builder()
+            .id(1L)
+            .title("Tiny Pretty Things")
+            .stock(5)
+            .build();
+
+        StudentEntity createOrderStudentEntity = StudentEntity.builder()
+            .id(1L)
+            .trustRate(100)
+            .email("filankes@gmail.com")
+            .finCode("5SFJ13D")
+            .firstName("Filankes")
+            .lastName("Filankesov")
+            .phoneNumber("050 550 50 50")
+            .build();
+
+        OrderEntity createOrderEntity = OrderEntity.builder().build(); 
+
+        OrderEntity createOrderEntityWithRelations = OrderEntity.builder()
+            .book(createOrderBookEntity)
+            .student(createOrderStudentEntity)
+            .orderTimestamp(orderTime)
+            .returnTimestamp(returnTime)
+            .build(); 
+
+        OrderEntity createdOrderEntity = OrderEntity.builder()
+            .id(1L)
+            .book(createOrderBookEntity)
+            .student(createOrderStudentEntity)
+            .orderTimestamp(orderTime)
+            .returnTimestamp(returnTime)
+            .build(); 
+
+        Mockito.when(orderMapper.requestToEntity(createOrderRequest)).thenReturn(createOrderEntity);
+        Mockito.when(bookService.getBookEntityById(1L)).thenReturn(createOrderBookEntity);
+        Mockito.when(studentService.getStudentEntityById(1L)).thenReturn(createOrderStudentEntity);
+
+        mockedLocalDateTime.when(LocalDateTime::now).thenReturn(orderTime);
+
+        Mockito.when(bookService.updateBookStock(1L, 4))
+            .thenReturn(
+                BaseResponse.<Void>builder()
+                    .status(HttpStatus.OK.value())
+                    .success(true)
+                    .message("Book stock updated successfully.")
+                    .build()
+            );
+
+        Mockito.when(orderRepository.save(createOrderEntityWithRelations)).thenReturn(createdOrderEntity);
+
+        BaseResponse<Void> serviceResponse = orderService.createOrder(createOrderRequest);
+
+        Mockito.verify(orderMapper, Mockito.times(1)).requestToEntity(createOrderRequest);
+        Mockito.verify(bookService, Mockito.times(1)).getBookEntityById(1L);
+        Mockito.verify(studentService, Mockito.times(1)).getStudentEntityById(1L);
+        Mockito.verify(bookService, Mockito.times(1)).updateBookStock(1L, 4);
+        Mockito.verify(orderRepository, Mockito.times(1)).save(createOrderEntityWithRelations);
+        Mockito.verifyNoMoreInteractions(bookService, studentService, orderRepository, orderMapper);
+        
+        Assertions.assertEquals(HttpStatus.CREATED.value(), serviceResponse.getStatus());
+        Assertions.assertEquals("Order created successfully.", serviceResponse.getMessage());
+        Assertions.assertTrue(serviceResponse.isSuccess());
+        Assertions.assertNull(serviceResponse.getData());
+
+        mockedLocalDateTime.close();
+    }
+
+    @Test
+    @DisplayName(value = "Testing createOrder() method when book and student exists but book stock is not enough")
+    public void givenCreateOrderWhenBookAndStudentExistsButBookStockIsNotEnoughThenThrowBookOutOfStockException()
+    {
+        LocalDateTime orderTime = LocalDateTime.now();
+        MockedStatic<LocalDateTime> mockedLocalDateTime = Mockito.mockStatic(LocalDateTime.class);
+
+        OrderRequest createOrderRequest = OrderRequest.builder()
+            .bookId(1L)
+            .studentId(1L)
+            .daysToReturn(0)
+            .build();
+
+        BookEntity createOrderBookEntity = BookEntity.builder()
+            .id(1L)
+            .title("Tiny Pretty Things")
+            .stock(0)
+            .build();
+
+        StudentEntity createOrderStudentEntity = StudentEntity.builder()
+            .id(1L)
+            .trustRate(100)
+            .email("filankes@gmail.com")
+            .finCode("5SFJ13D")
+            .firstName("Filankes")
+            .lastName("Filankesov")
+            .phoneNumber("050 550 50 50")
+            .build();
+
+        OrderEntity createOrderEntity = OrderEntity.builder().build(); 
+
+        Mockito.when(orderMapper.requestToEntity(createOrderRequest)).thenReturn(createOrderEntity);
+        Mockito.when(bookService.getBookEntityById(1L)).thenReturn(createOrderBookEntity);
+        Mockito.when(studentService.getStudentEntityById(1L)).thenReturn(createOrderStudentEntity);
+
+        mockedLocalDateTime.when(LocalDateTime::now).thenReturn(orderTime);
+
+        Mockito.when(bookService.updateBookStock(1L, -1))
+            .thenThrow(new BookOutOfStockException("Book is out of stock."));
+
+        Assertions.assertThrows(BookOutOfStockException.class, () -> orderService.createOrder(createOrderRequest));
+
+        Mockito.verify(orderMapper, Mockito.times(1)).requestToEntity(createOrderRequest);
+        Mockito.verify(bookService, Mockito.times(1)).getBookEntityById(1L);
+        Mockito.verify(studentService, Mockito.times(1)).getStudentEntityById(1L);
+        Mockito.verify(bookService, Mockito.times(1)).updateBookStock(1L, -1);
+        Mockito.verifyNoMoreInteractions(bookService, studentService, orderRepository, orderMapper);
+    
+        mockedLocalDateTime.close();
+    }
+
+    @Test
+    @DisplayName(value = "Testing createOrder() method when book exists but student doesn't")
+    public void givenCreateOrderWhenBookExistsButStudentDoesntExistThenThrowStudentNotFoundException()
+    {
+        OrderRequest createOrderRequest = OrderRequest.builder()
+            .bookId(1L)
+            .studentId(1L)
+            .daysToReturn(0)
+            .build();
+
+        BookEntity createOrderBookEntity = BookEntity.builder()
+            .id(1L)
+            .title("Tiny Pretty Things")
+            .stock(0)
+            .build();
+
+        OrderEntity createOrderEntity = OrderEntity.builder().build(); 
+
+        Mockito.when(orderMapper.requestToEntity(createOrderRequest)).thenReturn(createOrderEntity);
+        Mockito.when(bookService.getBookEntityById(1L)).thenReturn(createOrderBookEntity);
+        Mockito.when(studentService.getStudentEntityById(1L))
+            .thenThrow(new StudentNotFoundException("Student not found with id : 1"));
+
+        Assertions.assertThrows(StudentNotFoundException.class, () -> orderService.createOrder(createOrderRequest));
+
+        Mockito.verify(orderMapper, Mockito.times(1)).requestToEntity(createOrderRequest);
+        Mockito.verify(bookService, Mockito.times(1)).getBookEntityById(1L);
+        Mockito.verify(studentService, Mockito.times(1)).getStudentEntityById(1L);
+        Mockito.verifyNoMoreInteractions(bookService, studentService, orderRepository, orderMapper);
+    }
+
+    @Test
+    @DisplayName(value = "Testing createOrder() method when book doesn't exist")
+    public void givenCreateOrderWhenBookDoesNotExistThenThrowBookNotFoundException()
+    {
+        OrderRequest createOrderRequest = OrderRequest.builder()
+            .bookId(1L)
+            .studentId(1L)
+            .daysToReturn(0)
+            .build();
+
+        OrderEntity createOrderEntity = OrderEntity.builder().build(); 
+
+        Mockito.when(orderMapper.requestToEntity(createOrderRequest)).thenReturn(createOrderEntity);
+        Mockito.when(bookService.getBookEntityById(1L))
+            .thenThrow(new BookNotFoundException("Book not found with id : 1"));
+
+        Assertions.assertThrows(BookNotFoundException.class, () -> orderService.createOrder(createOrderRequest));
+
+        Mockito.verify(orderMapper, Mockito.times(1)).requestToEntity(createOrderRequest);
+        Mockito.verify(bookService, Mockito.times(1)).getBookEntityById(1L);
+        Mockito.verifyNoMoreInteractions(bookService, studentService, orderRepository, orderMapper);
     }
 }
